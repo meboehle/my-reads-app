3fa842fd371322344b6f4710fa8dcad5040f0565
Meg Boehlert <boehlertm@gmail.com>
2020-04-25 16:33:30 -0400

    feat: Routing for list books and search books components, refresh books func
    Setup routes for the two views: list books and search books. Refresh books view whenever component is updated (e.g. user moves book to a shelf)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import Bookshelf from './Bookshelf';
+import './App.css';
+import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
+import * as BooksAPI from './BooksAPI';
 
 class App extends Component {
+  mounted = false;
+  state = {
+    currentlyReadingBooks: [],
+    wantToReadBooks: [],
+    readBooks: [],
+    allBooks: []
+  }
+
+  refreshBooks() {
+    if (this.mounted) {
+      BooksAPI.getAll()
+        .then((books) => {
+          this.setState(() => ({
+            allBooks: books,
+            currentlyReadingBooks: books.filter(book => book.shelf === 'currentlyReading'),
+            wantToReadBooks: books.filter(book => book.shelf === 'wantToRead'),
+            readBooks: books.filter(book => book.shelf === 'read'),
+          }));
+        });
+    }
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    this.refreshBooks();
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  componentDidUpdate() {
+    if (this.mounted) this.refreshBooks();
+  }
+
   render() {
     return (
-      <div>
+      <div className='app'>
         <Route exact path='/' render={() => (
-          <Bookshelf/>
+          <ListBooks
+            currentlyReadingBooks={this.state.currentlyReadingBooks}
+            wantToReadBooks={this.state.wantToReadBooks}
+            readBooks={this.state.readBooks}
+          />
         )}
         />
-        <Route path='/search' render={({ history }) => (
-          <SearchBooks/>
+        <Route path='/search' render={() => (
+          <SearchBooks shelvedBooks={this.state.allBooks}/>
         )}
         />
       </div>
